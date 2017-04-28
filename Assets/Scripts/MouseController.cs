666b586d39c2e13747fc2a55dd4e0cc40cc4649b
PC09\Usuario09 <whitey.wings@gmail.com>
2017-04-28 18:21:50 +0200

    no message

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    //Variables
+    public LayerMask mask;
+    Interactable inter;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Lanzamos continuamente un rayo para controlar la posicion del raton
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, mask);
+
+        //Pintamos el rayo
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.magenta);
+        if (hit)
+        {
+            inter = hit.transform.GetComponent<Interactable>();
+            if (inter)
+            {
+                Debug.Log(inter.Info());
+            }
+        }
+    }
+}
